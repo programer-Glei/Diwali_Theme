3d6dc8cc076137998f6821f23caf7d43aa22c185
Gleibson Pereira <gleibson.santos05@gmail.com>
2024-04-12 17:59:56 -0300

    proximo comit

--- java/script.js
+++ java/script.js
@@ -1,1 +1,19 @@
+let header = document.querySelector('.header');
+let navbar = document.querySelector('.header .flex .navbar');
+let MenuBtn = document.querySelector('#menu-btn');
 
+MenuBtn.onclick = () =>{
+    navbar.classList.toggle('active');
+    MenuBtn.classList.toggle('fa-times');
+}
+
+window.onscroll = () =>{
+    navbar.classList.remove('active');
+    MenuBtn.classList.remove('fa-times');
+
+    if(window.scrollY > 0){
+        header.classList.add('active');
+    }else{
+        header.classList.remove('active');
+    }
+}
